6729c8c12517db27dfd0fbb84d9dd9d12706ca5c
aitfru23daw2 <464425.clot@fje.edu>
2024-04-03 16:59:49 +0200

    Commit tot casi menys modificar falta corregir

--- agregar.php
+++ agregar.php
@@ -6,54 +6,56 @@ use Laminas\Ldap\Ldap;
 ini_set('display_errors', 0);
 #Dades de la nova entrada
 #
-$uid = $_POST['uid'];
-$unorg = $_POST['unorg'];
-$num_id = $_POST['num_id'];
-$grup = $_POST['grup'];
-$dir_pers = $_POST['dir_pers'];
-$sh = $_POST['sh'];
-$cn = $_POST['cn'];
-$sn = $_POST['sn'];
-$nom = $_POST['nom'];
-$mobil = $_POST['mobil'];
-$adressa = $_POST['adressa'];
-$telefon = $_POST['telefon'];
-$titol = $_POST['titol'];
-$descripcio = $_POST['descripcio'];
-$objcl=array('inetOrgPerson','organizationalPerson','person','posixAccount','shadowAccount','top');
-#
-#Afegint la nova entrada
-$domini = 'dc=fjeclot,dc=net';
-$opcions = [
-    'host' => 'zend-aifrya.fjeclot.net',
-    'username' => "cn=admin,$domini",
-    'password' => 'fjeclot',
-    'bindRequiresDn' => true,
-    'accountDomainName' => 'fjeclot.net',
-    'baseDn' => 'dc=fjeclot,dc=net',
-];
-$ldap = new Ldap($opcions);
-$ldap->bind();
-$nova_entrada = [];
-Attribute::setAttribute($nova_entrada, 'objectClass', $objcl);
-Attribute::setAttribute($nova_entrada, 'uid', $uid);
-Attribute::setAttribute($nova_entrada, 'uidNumber', $num_id);
-Attribute::setAttribute($nova_entrada, 'gidNumber', $grup);
-Attribute::setAttribute($nova_entrada, 'homeDirectory', $dir_pers);
-Attribute::setAttribute($nova_entrada, 'loginShell', $sh);
-Attribute::setAttribute($nova_entrada, 'cn', $cn);
-Attribute::setAttribute($nova_entrada, 'sn', $sn);
-Attribute::setAttribute($nova_entrada, 'givenName', $nom);
-Attribute::setAttribute($nova_entrada, 'mobile', $mobil);
-Attribute::setAttribute($nova_entrada, 'postalAddress', $adressa);
-Attribute::setAttribute($nova_entrada, 'telephoneNumber', $telefon);
-Attribute::setAttribute($nova_entrada, 'title', $titol);
-Attribute::setAttribute($nova_entrada, 'description', $descripcio);
-$dn = 'uid='.$uid.',ou='.$unorg.',dc=fjeclot,dc=net';
-if($ldap->add($dn, $nova_entrada)) {
-    echo "Usuari creat";
-} else {
-    echo "Error en la creació de l'usuari";
+    if ($_POST['uid'] && $_POST['unorg'] ) {
+    $uid = $_POST['uid'];
+    $unorg = $_POST['unorg'];
+    $num_id = $_POST['num_id'];
+    $grup = $_POST['grup'];
+    $dir_pers = $_POST['dir_pers'];
+    $sh = $_POST['sh'];
+    $cn = $_POST['cn'];
+    $sn = $_POST['sn'];
+    $nom = $_POST['nom'];
+    $mobil = $_POST['mobil'];
+    $adreça = $_POST['adreça'];
+    $telefon = $_POST['telefon'];
+    $titol = $_POST['titol'];
+    $descripcio = $_POST['descripcio'];
+    $objcl=array('inetOrgPerson','organizationalPerson','person','posixAccount','shadowAccount','top');
+    #
+    #Afegint la nova entrada
+    $domini = 'dc=fjeclot,dc=net';
+    $opcions = [
+        'host' => 'zend-aifrya.fjeclot.net',
+        'username' => "cn=admin,$domini",
+        'password' => 'fjeclot',
+        'bindRequiresDn' => true,
+        'accountDomainName' => 'fjeclot.net',
+        'baseDn' => 'dc=fjeclot,dc=net',
+    ];
+    $ldap = new Ldap($opcions);
+    $ldap->bind();
+    $nova_entrada = [];
+    Attribute::setAttribute($nova_entrada, 'objectClass', $objcl);
+    Attribute::setAttribute($nova_entrada, 'uid', $uid);
+    Attribute::setAttribute($nova_entrada, 'uidNumber', $num_id);
+    Attribute::setAttribute($nova_entrada, 'gidNumber', $grup);
+    Attribute::setAttribute($nova_entrada, 'homeDirectory', $dir_pers);
+    Attribute::setAttribute($nova_entrada, 'loginShell', $sh);
+    Attribute::setAttribute($nova_entrada, 'cn', $cn);
+    Attribute::setAttribute($nova_entrada, 'sn', $sn);
+    Attribute::setAttribute($nova_entrada, 'givenName', $nom);
+    Attribute::setAttribute($nova_entrada, 'mobile', $mobil);
+    Attribute::setAttribute($nova_entrada, 'postalAddress', $adreça);
+    Attribute::setAttribute($nova_entrada, 'telephoneNumber', $telefon);
+    Attribute::setAttribute($nova_entrada, 'title', $titol);
+    Attribute::setAttribute($nova_entrada, 'description', $descripcio);
+    $dn = 'uid='.$uid.',ou='.$unorg.',dc=fjeclot,dc=net';
+    if($ldap->add($dn, $nova_entrada)) {
+        echo "Usuari creat";
+    } else {
+        echo "Error en la creació de l'usuari";
+    }
 }
 ?>
 
@@ -65,19 +67,19 @@ if($ldap->add($dn, $nova_entrada)) {
         <h2>Crear Usuari</h2>
         <form action="http://zend-aifrya.fjeclot.net/projecteAifrya/agregar.php" method="POST">
             UID: <input type="text" name="uid"><br>
-            Organizational Unit: <input type="text" name="unorg"><br>
-            Numerical ID: <input type="text" name="num_id"><br>
-            Group ID: <input type="text" name="grup"><br>
-            Directory: <input type="text" name="dir_pers"><br>
+            OU: <input type="text" name="unorg"><br>
+            ID Numèrica: <input type="text" name="num_id"><br>
+            ID Grup: <input type="text" name="grup"><br>
+            Directori Personal: <input type="text" name="dir_pers"><br>
             Shell: <input type="text" name="sh"><br>
             CN: <input type="text" name="cn"><br>
             SN: <input type="text" name="sn"><br>
             Nom: <input type="text" name="nom"><br>
-            Mobile: <input type="text" name="mobil"><br>
-            Adressa: <input type="text" name="adressa"><br>
-            Telefon: <input type="text" name="telefon"><br>
-            Titol: <input type="text" name="titol"><br>
-            Descripcio: <input type="text" name="descripcio"><br>
+            Mobil: <input type="text" name="mobil"><br>
+            Adreça: <input type="text" name="adreça"><br>
+            Telèfon: <input type="text" name="telefon"><br>
+            Títol: <input type="text" name="titol"><br>
+            Descripció: <input type="text" name="descripcio"><br>
             <input type="submit" value="Envia">
             <input type="reset" value="Neteja">
         </form>
--- modificar.php
+++ modificar.php
@@ -1,1 +1,72 @@
 <?php
+require 'vendor/autoload.php';
+use Laminas\Ldap\Attribute;
+use Laminas\Ldap\Ldap;
+
+
+ini_set('display_errors', 0);
+
+if ($_POST['uid'] && $_POST['unorg'] ) {
+   
+    $uid = $_POST['uid'];
+    $unorg = $_POST['unorg'];
+    $dn = 'uid=' . $uid . ',ou=' . $unorg . ',dc=fjeclot,dc=net';
+    
+    $atributModif = $_POST['atributModif'];
+    $nouValor = $_POST['nouValor'];
+
+    
+    $domini = 'dc=fjeclot,dc=net';
+    $opcions = [
+        'host' => 'zend-aifrya.fjeclot.net',
+        'username' => "cn=admin,$domini",
+        'password' => 'fjeclot',
+        'bindRequiresDn' => true,
+        'accountDomainName' => 'fjeclot.net',
+        'baseDn' => 'dc=fjeclot,dc=net',
+    ];
+    
+    // Connect to LDAP server
+    $ldap = new Ldap($opcions);
+    $$ldap = new Ldap($opcions);
+    
+    $ldap->bind();
+    $entrada = $ldap->getEntry($dn);
+    if ($entrada){
+        Attribute::setAttribute($entrada,$atributModif,$nouValor);
+        $ldap->update($dn, $entrada);
+        echo "Atribut modificat";
+    } else echo "<b>Aquesta entrada no existeix</b><br><br>";
+}
+?>
+<html>
+<head>
+    <title>Modificar Atributs Usuari</title>
+</head>
+<body>
+<h2>Modificar Atributs d'Usuari</h2>
+    <form action="http://zend-aifrya.fjeclot.net/projecteAifrya/modificar.php" method="POST">
+    UID: <input type="text" name="uid"><br>
+    Unitat Organitzativa: <input type="text" name="unorg"><br>
+    Atribut a modificar:
+    <select name="atributModif">
+        <option value="uidNumber">uidNumber</option>
+        <option value="gidNumber">gidNumber</option>
+        <option value="homeDirectory">Directori personal</option>
+        <option value="loginShell">Shell</option>
+        <option value="cn">cn</option>
+        <option value="sn">sn</option>
+        <option value="givenName">givenName</option>
+        <option value="postalAddress">PostalAdress</option>
+        <option value="mobile">Mobil</option>
+        <option value="telephoneNumber">Telefon</option>
+        <option value="title">Titol</option>
+        <option value="description">Descripcio</option>
+    </select><br>
+    Nou valor: <input type="text" name="nouValor"><br>
+    <input type="submit" value="Modificar">
+    <input type="reset" value="Neteja">
+</form>
+<a href="http://zend-aifrya.fjeclot.net/projecteAifrya/menu.php">Torna al menú</a>
+</body>
+</html>
